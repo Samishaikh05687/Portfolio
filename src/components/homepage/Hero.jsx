2b9a3a5fb7adcceb77c9558413514626410aebc0
sami shaikh <samishaikh05687@gmail.com>
2024-02-02 21:10:12 +0530

    change dependencies

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
 import { useRef, useEffect } from "react";
-import heroImg from '/src/assets/images/heroImg.webp';
+import heroImg from '/src/assets/images/heroimg.webp';
 
 
 export default function Hero() {
